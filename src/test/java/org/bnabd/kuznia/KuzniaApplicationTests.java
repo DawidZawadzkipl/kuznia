4779cee036e82c33758d63d6209e274e14890b79
DawidZawadzkipl <dawidzawadzki.002@gmail.com>
2026-05-22 18:19:56 +0200

    Initial commit

---
+++ src/test/java/org/bnabd/kuznia/KuzniaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bnabd.kuznia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuzniaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
